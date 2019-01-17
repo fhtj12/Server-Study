5eb34e97488ec301415767b1c82c031ff7aa3e98
fhtj12 <fhtj12@gmail.com>
2019-01-17 22:11:55 +0900

    클라 조금 수정

---
+++ chat_server/client/client/server/information.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.server
+{
+    class information
+    {
+        private const String protocol = "http";
+        private const String server_address = "localhost";
+        private const String server_port = "9503";
+
+        public String get_server_info()
+        {
+            return protocol + "://" + server_address + ":" + server_port + "/";
+        }
+    }
+}
