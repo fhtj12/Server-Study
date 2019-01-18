af7a3c7c1464a62d03c51ecda06353a171d0bc50
fhtj12 <fhtj12@gmail.com>
2019-01-18 20:01:35 +0900

    라우터 쪼개기 수정2

--- chat_server/server/app.js
+++ chat_server/server/app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-// 라우터 쪼개기
+// 라우터 모듈 쪼개기
 var chat = require('./routes/chat.js');
 var account = require('./routes/account.js');
 
-// 라우터 파일 쪼개기
+// 라우터 요청 쪼개기
 app.use('/chat', chat);
 app.use('/account', account);
 
